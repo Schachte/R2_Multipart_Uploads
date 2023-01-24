9e1c4d8c705f7883149d37682ab4b3f08d80c230
Ryan Schachte <schachte@cloudflare.com>
2023-01-24 14:33:55 -0800

    reworking test for failed multipart uploads

--- main.go
+++ main.go
@@ -5,36 +5,59 @@ import (
 	"io"
 	"log"
 	"os"
-	"time"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 const ACCESS_KEY = ""
 const SECRET_KEY = ""
 const ACCOUNT_ID = ""
-const SAMPLE_FILE = "sample.mov"
+const SAMPLE_FILE = ""
+const BUCKET_NAME = ""
 
 type S3WriteCloser struct {
 	Part        int
 	WriteCloser io.WriteCloser
 }
 
+type S3Metadata struct {
+	BucketName    string
+	Key           string
+	ContentLength int
+	wg            *sync.WaitGroup
+}
+
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	file, err := os.Open(SAMPLE_FILE)
 	if err != nil {
 		fmt.Println("Failed to open file", err)
 		return
 	}
-	defer file.Close()
+
+	// only necesary for PutObject requests that need content-length header
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println("Failed to open file", err)
+		return
+	}
 
 	uploader := getUploader(ACCOUNT_ID, ACCESS_KEY, SECRET_KEY)
-	closer := writer(uploader)
-	buffer := make([]byte, 1024*5)
+	closer := writer(uploader, S3Metadata{
+		BucketName:    BUCKET_NAME,
+		Key:           SAMPLE_FILE,
+		ContentLength: int(stat.Size()),
+		wg:            &wg,
+	})
 
+	// simulate range requests or chunking here
+	buffer := make([]byte, 1024*1024)
 	for {
 		_, err = file.Read(buffer)
 		if err != nil {
@@ -46,33 +69,35 @@ func main() {
 		}
 
 		n, err := closer.WriteCloser.Write(buffer)
+		closer.Part++
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Wrote %d bytes\n", n)
+		fmt.Printf("Wrote %d mb\n", n/1024)
 	}
 
 	closer.WriteCloser.Close()
-	time.Sleep(10 * time.Second)
+	fmt.Println("Waiting for Upload to complete...")
+	wg.Wait()
+	fmt.Println("Upload Completed")
 }
 
 // writer will be invoked for each part of the file that we want
 // to upload in parallel
-func writer(uploader *s3manager.Uploader) *S3WriteCloser {
+func writer(uploader *s3manager.Uploader, metadata S3Metadata) *S3WriteCloser {
 	r, w := io.Pipe()
 	writeCloser := S3WriteCloser{
 		Part:        1,
 		WriteCloser: w,
 	}
 
-	go func(uploader *s3manager.Uploader, reader *io.PipeReader) {
-		err := processUpload(uploader, r)
+	go func(uploader *s3manager.Uploader, reader *io.PipeReader, metadata S3Metadata) {
+		err := processUpload(uploader, r, metadata)
 		if err != nil {
 			log.Fatal("error uploading to R2", err)
 		}
-		fmt.Printf("Uploaded part: %d\n", writeCloser.Part)
-	}(uploader, r)
-
+		fmt.Printf("Uploaded a total of %d parts\n", writeCloser.Part)
+	}(uploader, r, metadata)
 	return &writeCloser
 }
 
@@ -81,20 +106,42 @@ func writer(uploader *s3manager.Uploader) *S3WriteCloser {
 func processUpload(
 	uploader *s3manager.Uploader,
 	reader *io.PipeReader,
+	metadata S3Metadata,
 ) error {
 	fmt.Println("Uploading to AWS")
-	uploadOptions := s3manager.UploadInput{
-		Bucket: aws.String("test-multipart"),
-		Key:    aws.String("sample4.mov"),
-		Body:   aws.ReadSeekCloser(reader),
+	upload := func() error {
+		_, err := uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(metadata.BucketName),
+			Key:    aws.String(metadata.Key),
+			Body:   aws.ReadSeekCloser(reader),
+		})
+		if err == nil {
+			metadata.wg.Done()
+		}
+		return err
 	}
 
-	_, err := uploader.Upload(&uploadOptions)
-	if err != nil {
-		fmt.Println("Failed to upload file", err)
+	// TODO: Add HEAD check to check existence of bucket instead of relying on failure
+	if err := handleBucketCreation(uploader, metadata.BucketName); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if err := upload(); err != nil {
+		fmt.Println(err)
 		return err
 	}
 	return nil
+}
+
+func handleBucketCreation(uploader *s3manager.Uploader, bucket string) error {
+	createBucketInput := &s3.CreateBucketInput{
+		CreateBucketConfiguration: &s3.CreateBucketConfiguration{
+			LocationConstraint: aws.String("hint:WNAM"),
+		},
+		Bucket: aws.String(bucket),
+	}
+	_, err := uploader.S3.CreateBucket(createBucketInput)
+	return err
 }
 
 // getUploader returns uploader instance to interface with R2
@@ -108,6 +155,5 @@ func getUploader(accountId, accessKey, secretKey string) *s3manager.Uploader {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	return s3manager.NewUploader(sess)
 }
